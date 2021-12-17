0d0696b355b4312aa72f625b8b949530bb279884
rrajak <rrajak@evoketechnologies.com>
2021-12-17 12:39:36 +0530

    sample lambda handler

---
+++ src/main/java/com/sample/SampleHandler.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
+public class SampleHandler implements RequestHandler<Map<String, String>, String> {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        logger.log("Environment Variables: " + gson.toJson(System.getenv()));
+        logger.log("Context: " + context);
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass());
+        return "200 OK - Sample Lambda";
+    }
+}
